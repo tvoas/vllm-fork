6a0df13981b96da3958643d6864ea42c98055fec
Tanner Voas <tanner.voas@intel.com>
2025-10-21 12:22:03 +0800

    Late truncate

--- vllm/executor/executor_base.py
+++ vllm/executor/executor_base.py
@@ -548,7 +548,7 @@ class DistributedExecutorBase(ExecutorBase):
                 if is_prompt and chunk_size > 0:
                     step = self._prefill_chunk_steps.get(seq_key, 0)
                     assert step > 0, "Prefill chunk step should be greater than zero."
-                    limits[seq_key] = ((step - 1) * chunk_size, step * chunk_size)
+                    limits[seq_key] = (0 if step == 1 else (step - 1) * chunk_size + 1, step * chunk_size + 1)
                 else:
                     limits[seq_key] = (0, 0)
         return limits
