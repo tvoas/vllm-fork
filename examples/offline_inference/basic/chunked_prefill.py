68975324c29932f1f681556895e4e495c3277fac
Tanner Voas <tanner.voas@intel.com>
2025-11-04 14:49:01 +0800

    TV: Fix YJ Chunk With PP

--- examples/offline_inference/basic/chunked_prefill.py
+++ examples/offline_inference/basic/chunked_prefill.py
@@ -1,5 +1,11 @@
 import os
-os.environ["VLLM_SKIP_WARMUP"] = "true"
+import asyncio
+from vllm import SamplingParams
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.entrypoints.openai.api_server import build_async_engine_client_from_engine_args
+from vllm.utils import merge_async_iterators
+
+os.environ["VLLM_SKIP_WARMUP"] = "false"
 os.environ['VLLM_CONTIGUOUS_PA'] = 'false'
 os.environ['VLLM_MLA_DISABLE_REQUANTIZATION']='1'
 os.environ['PT_HPU_ENABLE_LAZY_COLLECTIVES']='true'
@@ -10,41 +16,44 @@ os.environ['PT_HPU_LAZY_MODE']='1'
 os.environ['VLLM_DELAYED_SAMPLING']='false'
 #os.environ['VLLM_USE_V1']='1'
 
+async def main():
+    model_name = "/root/.cache/huggingface/GLM-4.5-FP8/"
+    engine_args = AsyncEngineArgs(
+        model=model_name,
+        trust_remote_code=True,
+        #enforce_eager=True, # Uncomment this to resolve error
+        dtype="bfloat16",
+        use_v2_block_manager=True,
+        tensor_parallel_size=4,
+        pipeline_parallel_size=2,
+        max_model_len=1024,
+        num_scheduler_steps=1,
+        gpu_memory_utilization=0.5,
+        max_num_seqs=2,
+        enable_chunked_prefill=True,
+        max_num_batched_tokens=128,
+        seed=2024
+    )
 
-if __name__ == "__main__":
-
-    from vllm import LLM, SamplingParams
-
-    # Sample prompts.
     prompts = [
         "Hello, my name is",
         "The president of the United States is",
         "The capital of France is",
         "The future of AI is",
     ]
-    # Create a sampling params object.
-    sampling_params = SamplingParams(temperature=0.0, max_tokens=128)
 
-    model_name = "/home/HF_models/llama-3-8b"
-    llm = LLM(model=model_name,
-            trust_remote_code=True,
-            enforce_eager=True,
-            dtype="bfloat16",
-            use_v2_block_manager=True,
-            tensor_parallel_size=1,
-            max_model_len=1024,
-            num_scheduler_steps=1,
-            gpu_memory_utilization=0.5,
-            max_num_seqs=128,
-            enable_chunked_prefill=True,
-            max_num_batched_tokens=128,
-            seed=2024)
-    # Generate texts from the prompts. The output is a list of RequestOutput objects
-    # that contain the prompt, generated text, and other information.
-    outputs = llm.generate(prompts, sampling_params)
-    # Print the outputs.
-    for output in outputs:
-        prompt = output.prompt
-        generated_text = output.outputs[0].text
-        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+    async with build_async_engine_client_from_engine_args(engine_args) as llm:
+        # Create a sampling params object.
+        sampling_params = SamplingParams(temperature=0.0, max_tokens=128)
+        # Generate texts from the prompts. The output is a list of RequestOutput objects
+        generators = []
+        for i, prompt in enumerate(prompts):
+            generator = llm.generate(prompt, sampling_params, request_id=f"test{i}")
+            generators.append(generator)
+        all_gens = merge_async_iterators(*generators)
+        async for i, res in all_gens:
+            if res.finished:
+                print(f"result: {res}")
 
+if __name__ == "__main__":
+    asyncio.run(main())
