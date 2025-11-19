f5b54ff624e634b8cde218b5925328a0d9e844f2
Tanner Voas <tanner.voas@intel.com>
2025-11-19 13:07:26 +0800

    TV: PP run_cluster.sh for HPU
    - Verified with results: GLM-4.5-Air-FP8-G2 no INC on G2D
    
    Signed-off-by: Tanner Voas <tanner.voas@intel.com>

--- examples/online_serving/run_cluster.sh
+++ examples/online_serving/run_cluster.sh
@@ -1,17 +1,51 @@
 #!/bin/bash
 
 # Check for minimum number of required arguments
+Help() {
+    # Display Help
+    echo "Usage: $0 docker_image head_node_address --head|--worker path_to_hf_home"
+    echo "       [-h] [-d hpu|gpu] [-c true|false]  [-n CONTAINER_NAME] [-- additional_args..."]
+}
+
 if [ $# -lt 4 ]; then
-    echo "Usage: $0 docker_image head_node_address --head|--worker path_to_hf_home [additional_args...]"
+    Help
     exit 1
 fi
 
-# Assign the first three arguments and shift them away
+# Assign the first four arguments and shift them away
 DOCKER_IMAGE="$1"
 HEAD_NODE_ADDRESS="$2"
 NODE_TYPE="$3"  # Should be --head or --worker
 PATH_TO_HF_HOME="$4"
 shift 4
+
+PLATFORM="gpu"
+CLEANUP_ON_EXIT="true"
+CONTAINER_NAME="node"
+
+# Get the options
+while getopts hd:c:n: flag; do
+    case $flag in
+    h) # display Help
+        Help
+        exit
+        ;;
+    d) # get the device type
+        PLATFORM=$OPTARG ;;
+    c) # get exit cleanup option
+        CLEANUP_ON_EXIT=$OPTARG ;;
+    n) # get container name
+        CONTAINER_NAME=$OPTARG ;;
+    \?) # Invalid option
+        echo "Error: Invalid option"
+        Help
+        exit
+        ;;
+    esac
+done
+
+# Shift the processed options and their arguments
+shift $((OPTIND - 1))
 
 # Additional arguments are passed directly to the Docker command
 ADDITIONAL_ARGS=("$@")
@@ -24,26 +58,47 @@ fi
 
 # Define a function to cleanup on EXIT signal
 cleanup() {
-    docker stop node
-    docker rm node
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
 }
-trap cleanup EXIT
+if [[ "$CLEANUP_ON_EXIT" == "true" ]]; then
+    trap cleanup EXIT
+fi
 
 # Command setup for head or worker node
 RAY_START_CMD="ray start --block"
 if [ "${NODE_TYPE}" == "--head" ]; then
-    RAY_START_CMD+=" --head --port=6379"
+    RAY_START_CMD+=" --head --node-ip-address ${HEAD_NODE_ADDRESS} --port=6379"
 else
     RAY_START_CMD+=" --address=${HEAD_NODE_ADDRESS}:6379"
 fi
 
 # Run the docker command with the user specified parameters and additional arguments
-docker run \
-    --entrypoint /bin/bash \
-    --network host \
-    --name node \
-    --shm-size 10.24g \
-    --gpus all \
-    -v "${PATH_TO_HF_HOME}:/root/.cache/huggingface" \
-    "${ADDITIONAL_ARGS[@]}" \
-    "${DOCKER_IMAGE}" -c "${RAY_START_CMD}"
+if [[ "$PLATFORM" == "hpu" ]]; then
+    docker run \
+        -td \
+        --entrypoint /bin/bash \
+        --network host \
+        --ipc=host \
+        --name $CONTAINER_NAME \
+        -e OMPI_MCA_btl_vader_single_copy_mechanism=none \
+        --cap-add SYS_NICE \
+        --privileged \
+        --runtime=habana \
+        -e HABANA_VISIBLE_DEVICES=all \
+        -e GLOO_SOCKET_IFNAME=${GLOO_SOCKET_IFNAME} \
+        -e HCCL_SOCKET_IFNAME=${HCCL_SOCKET_IFNAME} \
+        -v "${PATH_TO_HF_HOME}:/root/.cache/huggingface" \
+        "${ADDITIONAL_ARGS[@]}" \
+        "${DOCKER_IMAGE}" -c "${RAY_START_CMD}"
+else
+    docker run \
+        --entrypoint /bin/bash \
+        --network host \
+        --name $CONTAINER_NAME \
+        --shm-size 10.24g \
+        --gpus all \
+        -v "${PATH_TO_HF_HOME}:/root/.cache/huggingface" \
+        "${ADDITIONAL_ARGS[@]}" \
+        "${DOCKER_IMAGE}" -c "${RAY_START_CMD}"
+fi
